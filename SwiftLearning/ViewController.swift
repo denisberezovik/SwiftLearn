23b1d067b303909faede84a7a3ff21dc77718bf5
Grishkazzz <gr.zaulichny@insspring.com>
2021-07-15 09:54:54 +0300

    Initial Commit

---
+++ SwiftLearning/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftLearning
+//
+//  Created by Григорий Зауличный on 15.07.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
