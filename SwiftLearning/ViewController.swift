d588ee36af454087faca535726b5e24b0040d936
denisberezovik <denisberezovik@gmail.com>
2021-10-20 13:33:59 +0300

    UIStackView - Task1

--- SwiftLearning/ViewController.swift
+++ SwiftLearning/ViewController.swift
@@ -12,13 +12,20 @@ class ViewController: UIViewController {
     
     // MARK: - Properties
     var array = [String]()
-    var arrayInt: [Int] = [1,2,3,4,5,6,7,8,9,10]
+    var arrayInt = [Int]()
     
     // MARK: - Subviews
-    var inputTextField = UITextField()
     var outputLabel = UILabel()
     var applyButton = UIButton()
     var removeButton = UIButton()
+    var oneTextField = UITextField()
+    var twoTextField = UITextField()
+    var threeTextField = UITextField()
+    var fourTextField = UITextField()
+    var stackView = UIStackView()
+    
+    lazy var safeArea = view.safeAreaLayoutGuide
+    
     
     // MARK: - Initializers
     
@@ -32,6 +39,7 @@ class ViewController: UIViewController {
         
         super.viewDidLoad()
         setup()
+        self.view.backgroundColor = #colorLiteral(red: 0.721568644, green: 0.8862745166, blue: 0.5921568871, alpha: 1)
     }
     
     public override func viewWillAppear(_ animated: Bool) {
@@ -40,42 +48,84 @@ class ViewController: UIViewController {
     
     // MARK: -  Methods
     
-    func setupTextField(textField: UITextField) {
-        textField.delegate = self
+    
+    func configureStackView() {
+        stackView.axis = .vertical
+        stackView.distribution = .fillEqually
+        stackView.spacing = 6
+        
+    }
+    
+    func setStackViewConstraints() {
+        configureStackView()
+        
+        stackView.backgroundColor = #colorLiteral(red: 0.721568644, green: 0.8862745166, blue: 0.5921568871, alpha: 1)
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.topAnchor.constraint(equalTo: safeArea.topAnchor, constant: 20).isActive = true
+        stackView.leadingAnchor.constraint(equalTo: outputLabel.leadingAnchor).isActive = true
+        stackView.trailingAnchor.constraint(equalTo: outputLabel.trailingAnchor).isActive = true
+        stackView.bottomAnchor.constraint(equalTo: applyButton.topAnchor, constant: -10).isActive = true
+        
     }
     
     private func setup() {
-        configureSubviews()
         buildHierarchy()
+        setStackViewConstraints()
+        configureSubviews()
         layoutSubviews()
-        setupTextField(textField: inputTextField)
     }
     
     private func buildHierarchy() {
-        view.addSubview(inputTextField)
+        view.addSubview(stackView)
+        
+        stackView.addArrangedSubview(oneTextField)
+        stackView.addArrangedSubview(twoTextField)
+        stackView.addArrangedSubview(threeTextField)
+        stackView.addArrangedSubview(fourTextField)
+        
         view.addSubview(outputLabel)
         view.addSubview(applyButton)
         view.addSubview(removeButton)
     }
     
+    func setupTextField(textField: UITextField, color: UIColor) {
+        
+        textField.delegate = self
+        textField.borderStyle = .roundedRect
+        textField.keyboardType = .numberPad
+        textField.backgroundColor = color
+        
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.widthAnchor.constraint(equalTo: stackView.widthAnchor).isActive = true
+        
+        oneTextField.addTarget(self, action: #selector(textChanged(_:)), for: .editingChanged)
+        twoTextField.addTarget(self, action: #selector(textChanged(_:)), for: .editingChanged)
+        threeTextField.addTarget(self, action: #selector(textChanged(_:)), for: .editingChanged)
+        fourTextField.addTarget(self, action: #selector(textChanged(_:)), for: .editingChanged)
+        
+    }
+    
+    
     private func configureSubviews() {
         applyButton.layer.cornerRadius = 10
-        applyButton.backgroundColor = .systemPink
+        applyButton.backgroundColor = UIColor(hexString: "#5ac18e")
         applyButton.setTitle("Apply".uppercased(), for: .normal)
         applyButton.addTarget(self, action: #selector(applyButtonTapped), for: .touchUpInside)
+        applyButton.isHidden = true
         
-        inputTextField.borderStyle = .roundedRect
-        inputTextField.backgroundColor = .yellow
-        inputTextField.keyboardType = .numberPad
+        setupTextField(textField: oneTextField, color: UIColor(hexString: "#faebd7"))
+        setupTextField(textField: twoTextField, color: UIColor(hexString: "#faebd7"))
+        setupTextField(textField: threeTextField, color: UIColor(hexString: "#faebd7"))
+        setupTextField(textField: fourTextField, color: UIColor(hexString: "#faebd7"))
         
         removeButton.layer.cornerRadius = 10
-        removeButton.backgroundColor = .red
+        removeButton.backgroundColor = UIColor(hexString: "#7fffd4")
         removeButton.setTitleColor(.black, for: .normal)
-        removeButton.setTitle("Remove".uppercased(), for: .normal)
+        removeButton.setTitle("Multiple".uppercased(), for: .normal)
         removeButton.addTarget(self, action: #selector(removeButtonTapped), for: .touchUpInside)
         
-        outputLabel.backgroundColor = .gray
-        outputLabel.textColor = .white
+        outputLabel.backgroundColor = UIColor(hexString: "#ffff66")
+        outputLabel.textColor = .black
         outputLabel.font = .systemFont(ofSize: 15)
         outputLabel.numberOfLines = 4
         outputLabel.layer.cornerRadius = 10
@@ -84,104 +134,134 @@ class ViewController: UIViewController {
     }
     
     private func layoutSubviews() {
-        let safeArea = self.view.safeAreaLayoutGuide
-        
-        inputTextField.translatesAutoresizingMaskIntoConstraints = false
-        inputTextField.topAnchor.constraint(equalTo: safeArea.topAnchor, constant: 100).isActive = true
-        inputTextField.widthAnchor.constraint(equalTo: safeArea.widthAnchor, multiplier: 0.5).isActive = true
-        inputTextField.centerXAnchor.constraint(equalTo: safeArea.centerXAnchor).isActive = true
         
         applyButton.translatesAutoresizingMaskIntoConstraints = false
-        applyButton.widthAnchor.constraint(equalTo: inputTextField.widthAnchor).isActive = true
-        applyButton.centerYAnchor.constraint(equalTo: safeArea.centerYAnchor).isActive = true
+        applyButton.widthAnchor.constraint(equalTo: outputLabel.widthAnchor).isActive = true
         applyButton.centerXAnchor.constraint(equalTo: safeArea.centerXAnchor).isActive = true
-        applyButton.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        applyButton.heightAnchor.constraint(equalToConstant: 60).isActive = true
+        applyButton.bottomAnchor.constraint(equalTo: removeButton.topAnchor, constant: -10).isActive = true
         
         removeButton.translatesAutoresizingMaskIntoConstraints = false
-        removeButton.widthAnchor.constraint(equalTo: inputTextField.widthAnchor).isActive = true
-        removeButton.centerYAnchor.constraint(equalTo: safeArea.centerYAnchor, constant: 50).isActive = true
+        removeButton.widthAnchor.constraint(equalTo: outputLabel.widthAnchor).isActive = true
         removeButton.centerXAnchor.constraint(equalTo: safeArea.centerXAnchor).isActive = true
-        removeButton.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        removeButton.heightAnchor.constraint(equalToConstant: 60).isActive = true
+        removeButton.bottomAnchor.constraint(equalTo: outputLabel.topAnchor, constant: -10).isActive = true
         
         outputLabel.translatesAutoresizingMaskIntoConstraints = false
         outputLabel.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor, constant: 20).isActive = true
         outputLabel.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor, constant: -20).isActive = true
-        outputLabel.topAnchor.constraint(equalTo: inputTextField.bottomAnchor, constant: 40).isActive = true
-        outputLabel.heightAnchor.constraint(greaterThanOrEqualToConstant: 60).isActive = true
+        outputLabel.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor, constant: -10).isActive = true
+        outputLabel.topAnchor.constraint(equalTo: safeArea.centerYAnchor, constant: 100).isActive = true
         
     }
     
     
     // MARK: - Handlers
+    func appendInt(textField: UITextField) {
+        if let text = textField.text {
+            if let int = Int(text) {
+                arrayInt.append(int) }
+        }
+        
+        
+    }
+    
+    func clearFields() {
+        oneTextField.text = ""
+        twoTextField.text = ""
+        threeTextField.text = ""
+        fourTextField.text = ""
+    }
     
     @objc private func applyButtonTapped() {
         
-        arrayInt = [121,232,24231,13412,141324,1234231,13,4,6,7,8,4564,455]
+        appendInt(textField: oneTextField)
+        appendInt(textField: twoTextField)
+        appendInt(textField: threeTextField)
+        appendInt(textField: fourTextField)
         
-        if arrayInt.count > 1 {
-            
-            removeButton.isHidden = false
-            
-        }
+        view?.endEditing(true)
         
-        /* Задача 1
-        array.append(inputTextField.text ?? "empty")
-        inputTextField.text = ""
-        outputLabel.text = "\(array)"
-        */
-    
-        /* Задача 2
-        array.insert(inputTextField.text!, at:0)
-        inputTextField.text = ""
-        outputLabel.text = "\(array)"
-        */
+        //        arrayInt = [Int(oneTextField.text ?? "0") ?? 0, Int(twoTextField.text ?? "0") ?? 0, Int(threeTextField.text ?? "0") ?? 0, Int(fourTextField.text ?? "0") ?? 0]
+        //
         
-        /* Задача 3
-        array.append(inputTextField.text ?? "empty")
-        inputTextField.text = ""
-        outputLabel.text = (array.randomElement()!)
-         */
-        
-        /* Задача 4
-         if let number = inputTextField.text {
-            arrayInt.append(Int(number) ?? 0)
-        }
-        inputTextField.text = ""
-        outputLabel.text = "\(arrayInt)"
-         */
+        outputLabel.text = arrayInt.description
         
     }
-    
     
     @objc private func removeButtonTapped() {
         
-        let element = Int(inputTextField.text ?? "0")
-        let lastElement = arrayInt.last
-        
-        if element ?? 0 > lastElement ?? 0 {
-            arrayInt.removeLast()
-        } else {
-            arrayInt.removeFirst()
+        arrayInt = arrayInt.map { num in
+            num * 7
         }
+       
+        outputLabel.text = arrayInt.description
+        clearFields()
+        //print(arrayInt)
         
-        if arrayInt.count <= 1 {
-            
-            removeButton.isHidden = true
-            
-        }
+        //        let multiArray =
         
-        //button.isHidden = true
-        
-        
-        outputLabel.text = "\(arrayInt)"
     }
+    /* Задача 1
+     array.append(inputTextField.text ?? "empty")
+     inputTextField.text = ""
+     outputLabel.text = "\(array)"
+     */
+    
+    /* Задача 2
+     array.insert(inputTextField.text!, at:0)
+     inputTextField.text = ""
+     outputLabel.text = "\(array)"
+     */
+    
+    /* Задача 3
+     array.append(inputTextField.text ?? "empty")
+     inputTextField.text = ""
+     outputLabel.text = (array.randomElement()!)
+     */
+    
+    /* Задача 4
+     if let number = inputTextField.text {
+     arrayInt.append(Int(number) ?? 0)
+     }
+     inputTextField.text = ""
+     outputLabel.text = "\(arrayInt)"
+     */
+    
+    
     
 }
+
+
+
+/* Задача 5
+ let element = Int(inputTextField.text ?? "0")
+ let lastElement = arrayInt.last
+ 
+ if element ?? 0 > lastElement ?? 0 {
+ arrayInt.removeLast()
+ } else {
+ arrayInt.removeFirst()
+ }
+ 
+ if arrayInt.count <= 1 {
+ 
+ removeButton.isHidden = true
+ 
+ }
+ 
+ //button.isHidden = true
+ 
+ 
+ outputLabel.text = "\(arrayInt)"*/
+
+
+// MARK: -- SomeProtocol
 
 extension ViewController: UITextFieldDelegate {
     
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
-
+        
         let allowedCharacters = CharacterSet.decimalDigits
         let characterSet = CharacterSet(charactersIn: string)
         return allowedCharacters.isSuperset(of: characterSet)
@@ -191,5 +271,12 @@ extension ViewController: UITextFieldDelegate {
         textField.endEditing(true)
     }
     
+    @objc func textChanged(_ textField: UITextField) {
+        applyButton.isHidden = [oneTextField, twoTextField, threeTextField, fourTextField].contains { $0.text!.isEmpty }
+    }
+    
+    
 }
+        
+
 
